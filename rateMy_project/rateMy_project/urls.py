092a61ca6ad1314ed5fed3540bd1ad2779fe3bab
Mustafa Kurugollu <mkurugollu220@gmail.com>
2018-03-11 15:07:32 +0000

    changed url of home page

--- rateMy_project/rateMy_project/urls.py
+++ rateMy_project/rateMy_project/urls.py
@@ -24,7 +24,7 @@ from registration.backends.simple.views import RegistrationView
 
 class MyRegistrationView(RegistrationView):
     def get_success_url(self, user):
-        return '/ratemy/'
+        return '/home/'
 
 
 urlpatterns = [
