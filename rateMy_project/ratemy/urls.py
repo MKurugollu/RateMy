6f7b1b628f2ff3c36aad1c8f93ee01738451f86f
Mustafa Kurugollu <mkurugollu220@gmail.com>
2018-03-11 15:02:54 +0000

    Fixed Url mappings

--- rateMy_project/ratemy/urls.py
+++ rateMy_project/ratemy/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from ratemy import views
 
 urlpatterns = [
+    url(r'^$', views.home, name='home'),
     url(r'about/$', views.about, name='about'),
     url(r'faq/$', views.faq, name='faq'),
     url(r'home/$', views.home, name='home'),
