283bf38053ed652fe45263c412b6eab62566f269
Mustafa Kurugollu <mkurugollu220@gmail.com>
2018-03-22 13:06:19 +0000

    some changes

--- rateMy_project/ratemy/templatetags/ratemy_template_tags.py
+++ rateMy_project/ratemy/templatetags/ratemy_template_tags.py
@@ -1,8 +1,14 @@
 from django import template
-from ratemy.models import Category
+from ratemy.models import Category, UserProfile
+from django.contrib.auth.models import User
 
 register = template.Library()
 
 # @register.inclusion_tag('rango/cats.html')
 # def get_category_list(cat=None):
 #     return {'cats': Category.objects.all(), 'act_cat': cat}
+
+@register.inclusion_tag('ratemy/get_profile_pic.html')
+def find_userprofile():
+    userprofile = UserProfile.objects.all()
+    return {'userprofile': userprofile}
